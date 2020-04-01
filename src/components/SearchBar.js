71ffca91aab3730201b884fa84422037877a2dbb
Roger Mayer <rmayer1984@gmail.com>
2020-04-01 11:07:48 -0500

    prevented state object from being undefined with event onFormSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,16 @@ import React from "react";
 class SearchBar extends React.Component {
   state = {term: ''};
 
+
+    onFormSubmit = (event) =>{
+        event.preventDefault();
+        console.log(this.state.term);
+    };
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form" action="">
+                <form onSubmit={this.onFormSubmit} className="ui form" action="">
                     <div className="field">
                         <label>Image Search</label>
                     <input type="text"
@@ -22,3 +28,5 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+
+
