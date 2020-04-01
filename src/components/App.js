ad8706d3273462304462cc57432291c79a4ec0e1
Roger Mayer <rmayer1984@gmail.com>
2020-04-01 15:40:20 -0500

    bound callback. searchbar returning length of api query array

--- src/components/App.js
+++ src/components/App.js
@@ -12,15 +12,26 @@ import SearchBar from "./SearchBar";
 
 // class component
 class App extends React.Component {
-    onMySearchSubmit(term){
-        console.log(term);
-    }
+    state = {images: [] };
+
+   onMySearchSubmit = async term =>{
+       const response = await axios
+           .get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization:
+                    'Client-ID 2WLCoqH7U8jBo-CG_NdF_8YCJi2gx1Yz_KgnMZoUqyo'
+            }
+        });
+       this.setState({images: response.data.results});
+   }
 
 
     render () {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
             <SearchBar onSubmit={this.onMySearchSubmit}/>
+            Found: {this.state.images.length} images
             </div>
         );
     }
