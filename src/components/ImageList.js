15a7778155bd58429cffd8309fa39cfeb7401543
Roger Mayer <rmayer1984@gmail.com>
2020-04-02 10:08:36 -0500

    now returning images from api

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 const ImageList = (props) => {
+    const images = props.images.map((image) => {
+       return <img src={image.urls.regular} alt='pic' />
+    });
 
-    return <div>ImageList</div>
+
+    return <div>{images}</div>
 };
 
 export default ImageList;
