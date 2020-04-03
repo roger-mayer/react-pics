078212635d4afe5a9ed55be22cfee8a857544867
Roger Mayer <rmayer1984@gmail.com>
2020-04-03 10:56:25 -0500

    card showing. going to create new component with standard card size

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
+import './ImageList.css';
 import React from "react";
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-       return <img src={image.urls.regular} alt='pic' />
+    const images = props.images.map(({description, id, urls}) => {
+       return <img key={id} src={urls.regular} alt={description} />
     });
 
 
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 };
 
 export default ImageList;
