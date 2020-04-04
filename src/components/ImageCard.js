f8287161209fb175a5b8cd07fe4b94b7252875ae
Roger Mayer <rmayer1984@gmail.com>
2020-04-04 09:42:08 -0500

    standardizing image card spans

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.imageRef = React.createRef();
+    }
+
+    componentDidMount() {
+        console.log(this.imageRef);
+    }
+
     render() {
+        const { description, urls } = this.props.images;
+
         return (
             <div>
-                <img src={this.props.image.urls.regular}
-                     alt={this.props.image.description}
+                <img
+                    ref={this.imageRef}
+                    src={urls.regular}
+                    alt={description}
                 />
             </div>
         );
